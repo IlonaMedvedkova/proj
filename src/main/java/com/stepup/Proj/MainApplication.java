7c1cd26d7125e88bd12b8ced8dac7714a99b2eee
Ilona Medvedkova <ilonkax@yandex.ru>
2025-03-02 22:03:10 +0400

    Добавлен вызов метода doSomething

--- src/main/java/com/stepup/Proj/MainApplication.java
+++ src/main/java/com/stepup/Proj/MainApplication.java
@@ -4,6 +4,7 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("1121");
+        doSomething();
         System.out.println("---");
     }
     public static void doSomething() {
