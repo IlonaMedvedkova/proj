1f3483af5846d202ee36de6bbfc1985d71dc9c63
Ilona Medvedkova <ilonkax@yandex.ru>
2025-03-02 15:50:23 +0400

    доарботка логики проекта

--- src/main/java/com/stepup/Proj/MainApplication.java
+++ src/main/java/com/stepup/Proj/MainApplication.java
@@ -3,5 +3,6 @@ package com.stepup.Proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("1111");
     }
 }
