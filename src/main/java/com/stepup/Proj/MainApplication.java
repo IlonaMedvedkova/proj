5636b658d62519b76da3ca585746bba33e74da6a
Ilona Medvedkova <ilonkax@yandex.ru>
2025-03-02 22:02:25 +0400

    Добавили

--- src/main/java/com/stepup/Proj/MainApplication.java
+++ src/main/java/com/stepup/Proj/MainApplication.java
@@ -7,9 +7,7 @@ public class MainApplication {
         System.out.println("---");
     }
     public static void doSomething() {
-        System.out.println(0);
-        System.out.println(1);
-        System.out.println(1);
+        System.out.println("Heelo World!");
     }
 }
 
